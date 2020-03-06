e1547edfddae969e4c2c674b9980653ae7f69071
Max Huggins <max.huggins98@gmail.com>
2020-03-06 11:13:43 -0600

    Not even really sure

--- On_Ground_Testing/Code/FirstOOP.py
+++ On_Ground_Testing/Code/FirstOOP.py
@@ -24,12 +24,16 @@ class MCP3008:
      self.DOUT = DOUT
      self.DIN = DIN
 """
+V_ref = 5.0
+
 C = 0
 CS = GD['GPIO12']
 CLK = GD['GPIO16']
 DOUT = GD['GPIO21']
 DIN = GD['GPIO20']
+OE = GD['GPIO26']
 
+GPIO.setup(OE, OUT)
 GPIO.setup(CS, OUT)
 GPIO.setup(CLK, OUT)
 GPIO.setup(DOUT, IN)
@@ -98,11 +102,14 @@ def readMCP(C, CS, CLK, DOUT, DIN):
 def calc_voltsMCP(d):
     d_int = int(d,2)
     volts = V_ref*d_int / 1023
-    volts = round(volts, 3)
+    #volts = round(volts, 6)
     return volts
 
 try:
+     GPIO.output(OE, True)
      while True:
           voltage = calc_voltsMCP(readMCP(C, CS, CLK, DOUT, DIN))
           print(voltage)
           time.sleep(.1)
+finally:
+    GPIO.cleanup()
