4e64ec764cef79cba8432517a74a552f73cdc94c
Max Huggins <max.huggins98@gmail.com>
2020-03-12 00:16:51 -0500

    Fixed the MCP3008 library.

--- On_Ground_Testing/Code/ADCTesting.py
+++ On_Ground_Testing/Code/ADCTesting.py
@@ -2,33 +2,30 @@ import RPi.GPIO as RGPIO
 import time
 import numpy as np
 from GPIOTranslator import GPIODictionary as GD
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_MCP3008 as MCP
+import SPI
+import mcp3008 as MCP
 #============================================================================#
 
 #============================================================================#
-C = 0
-CS = 12 #Pin 10 on MCP3008
-CLK = 16 #Pin 13 on MCP3008
-MOSI = 20 #Pin 12 on MCP3008
-MISO = 21 #Pin 11 on MCP3008
+##C = 0
+##CS = 12 #Pin 10 on MCP3008
+##CLK = 16 #Pin 13 on MCP3008
+##MOSI = 20 #Pin 12 on MCP3008
+##MISO = 21 #Pin 11 on MCP3008
 OE = 26
 
-RGPIO.setmode(GPIO.BCM)
-RGPIO.setup(OE, GPIO.OUT)
+RGPIO.setmode(RGPIO.BCM)
+RGPIO.setup(OE, RGPIO.OUT)
 RGPIO.output(OE, True)
 
-mcp = MCP.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = MCP.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+#mcp = MCP.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 #============================================================================#
 ref = 5.12
-def calc_volts(d, AoM, ref):
-    d_int = int(d,2)
-    if AoM == 'ADC':
-        volts = ref*d_int / 256
-        volts = round(volts, 2)
-    if AoM == 'MCP':
-        volts = ref*d_int / 1024
-        volts = round(volts, 2)
+def calc_volts(d_int):
+    volts = ref*d_int / 1023
     return volts
 
 #============================================================================#
@@ -46,9 +43,18 @@ def calc_temp(voltage):
      return T
 #============================================================================#
 
-while True:
-    d =  mcp.read_adc(0)
-    voltage = calc_volts(d, 'MCP', ref)
-    pressure = calc_pressure(voltage)
-    print(pressure)
-    time.sleep(0.5)
+try:
+    while True:
+        d =  mcp.read_adc(0)
+        print(d)
+        voltage = calc_volts(d)
+        pressure = calc_pressure(voltage)
+        print(pressure)
+
+except KeyboardInterrupt:
+    print('great job... you made toast')
+
+finally:
+    RGPIO.cleanup()
+    print('Isaac cleaned the oven...')
+
